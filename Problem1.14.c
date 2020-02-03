7da80fe7575f6d2ebee07eb6955af5b6e887320e
valmartnz <47126912+valmartnz@users.noreply.github.com>
2020-02-03 15:23:37 -0800

    Update Problem1.14.c

--- Problem1.14.c
+++ Problem1.14.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
-#include <ctype.h>
 
-#define OUT 0
-#define IN 1
-#define MAX_WORD_LENGTH 25
+#define MAX_CHAR_LENGTH 128
 
 int main(int argc, const char* argv[]){
-    int c, nc;
-    nc = OUT;
+    int c;
     
-    int words[MAX_WORD_LENGTH];
-    for(int i = 0; i < MAX_WORD_LENGTH; i++)
-        words[i] = 0;
+    int chars[MAX_CHAR_LENGTH];
+    for(int i = 0; i < MAX_CHAR_LENGTH; i++)
+        chars[i] = 0;
         
-    while((c = getchar()) != EOF){
-        ++words[c];
-    }
+    while((c = getchar()) != EOF)
+        ++chars[c];
     
-    printf("Word Frequencies\n");
-    for(int i = 0; i < MAX_WORD_LENGTH; i++){
+    printf("Character Frequencies\n");
+    for(int i = 0; i < MAX_CHAR_LENGTH; i++){
         putchar(i);
-        for(int j = 0; j < words[i]; j++){
+        
+        for(int j = 0; j < chars[i]; j++){
             printf("*");
         }
         printf("\n");
